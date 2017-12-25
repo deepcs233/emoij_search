081933308dc0427a1456eb14813be5a43889dad1
邵昊 <shaohao@shaohaodeMacBook-Pro.local>
2017-12-25 18:52:08 +0800

    update

--- pHash.py
+++ pHash.py
@@ -1,4 +1,5 @@
 #encoding=utf-8
+from PIL import Image, ImageSequence
 import cv2
 import numpy as np
 from compiler.ast import flatten
@@ -8,9 +9,32 @@ def pHash(imgfile):
     """get image pHash value"""
     #加载并调整图片为32x32灰度图片
     img=cv2.imread(imgfile, 0)
+    return _getHash(img)
+
+def diff(img1, img2):
+    num = 0
+    for i in range(len(img1)):
+        if img1[i] != img2[i]:
+            num += 1
+    return num
+
+
+def pHashGif(imgfile):
+    with Image.open(imgfile) as im:
+        if im.is_animated:
+            frames = [f.copy() for f in ImageSequence.Iterator(im)]
+            frames_num = len(frames)
+            hash1 = _getHash(frames[0])
+            hash2 = _getHash(frames[frames_num / 2])
+            hash3 = _getHash(frames[-1])
+            return hash1 + hash2 + hash3
+        else:
+            return ''
+
+def _getHash(img_obj):
     img=cv2.resize(img,(64,64),interpolation=cv2.INTER_CUBIC)
 
-        #创建二维列表
+    #创建二维列表
     h, w = img.shape[:2]
     vis0 = np.zeros((h,w), np.float32)
     vis0[:h,:w] = img       #填充数据
@@ -29,10 +53,3 @@ def pHash(imgfile):
 
     #得到哈希值
     return ''.join(['%x' % int(''.join(avg_list[x:x+4]),2) for x in range(0,32*32,4)])
-
-def diff(img1, img2):
-    num = 0
-    for i in range(len(img1)):
-        if img1[i] != img2[i]:
-            num += 1
-    return num
